68e1fc7758609cfa19d4c5d7ff7d70a03796d1f1
Ziru Zou <jiyucoding@yahoo.com>
2023-01-15 13:00:10 -0500

    send data - a post requst using fetch()

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your code here
+function submitData (userName, userEmail) {
+    const formData = {
+        name: userName,
+        email: userEmail,
+    }
+    const destinationURL = " http://localhost:3000/users"
+    const configurationObject = {
+        header: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData)
+    }
+    fetch(destinationURL, configurationObject)
+}
